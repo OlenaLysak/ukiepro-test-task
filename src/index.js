982c8d3a429ab481cf85737e47ef9b49deccbdbb
OlenaLysak <os.lysak@gmail.com>
2023-08-02 14:59:33 +0300

    cleaned up unused files

--- src/index.js
+++ src/index.js
@@ -4,16 +4,13 @@ import './index.css';
 import App from './App';
 
 //Context
-import MyContextProvider from './context/MyContext';
 import { ThemeProvider } from './context/ThemeContext';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <MyContextProvider>
-      <ThemeProvider>
-        <App />
-      </ThemeProvider>
-    </MyContextProvider>
+    <ThemeProvider>
+      <App />
+    </ThemeProvider>
   </React.StrictMode>
 );
